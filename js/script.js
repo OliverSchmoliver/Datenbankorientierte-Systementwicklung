06aa4ffbb6aaccdd50ca5dc17797c48be0ba1131
OliverSchmoliver <168073586+OliverSchmoliver@users.noreply.github.com>
2024-06-10 17:10:59 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const burger = document.querySelector('.burger');
+    const menu = document.getElementById('menu');
+
+    burger.addEventListener('click', () => {
+        menu.classList.toggle('nav-active');
+        burger.classList.toggle('toggle');
+
+        burger.classList.toggle('active');
+        menu.classList.toggle('active');
+    });
+    countdown();
+});
+
+function countdown(){
+var countDownDate = new Date("July 1, 2024 00:00:00").getTime();
+var x = setInterval(function() {
+var now = new Date().getTime();
+var distance = countDownDate - now;
+var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+document.getElementById("countdown").innerHTML = days + " Days " + hours + " Hours "
++ minutes + " Minutes " + seconds + " Seconds ";
+
+if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown").innerHTML = "READY FOR TAKE OFF! is out now!";
+}
+}, 1000);
+}
